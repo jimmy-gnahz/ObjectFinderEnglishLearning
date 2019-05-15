c01f07c961ebc1531a63618d5a1d8002e22e0190
jimmy-gnahz <jimmyjimbozhang@gmail.com>
2019-05-15 16:33:07 +0800

    polished up the system for two words

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,33 +10,45 @@ public class GameManager : MonoBehaviour
 
     public static string currentLockedObjectTag;
     public GameObject FPC;
-    public GameObject WhatIsThisText;
-    private ArrayList wordlist;
+    public Text MessageText;
+    public static ArrayList wordlist;
     private ArrayList buttonList;
 
     public void Start()
     {
         buttonList = new ArrayList() { firstWord, secondWord };
-        wordlist = new ArrayList(){ "Tree", "Flower"};
+        wordlist = new ArrayList(){"Tree", "Flower"};
     }
 
     public void CheckIfCorrect(Text buttonText)
     {
+        // Selected the right word
         for(int i=0; i<wordlist.Count; i++){
             if (GameManager.currentLockedObjectTag == (string) wordlist[i] &&
                 GameManager.currentLockedObjectTag == buttonText.text)
             {
-                WhatIsThisText.SetActive(false);
+                StartCoroutine(ShowMessage("Select the word.", 2));
                 Debug.Log(wordlist[i]);
                 Button currentButton = (Button) buttonList[i];
                 Destroy(currentButton);
                 wordlist.RemoveAt(i);
+                buttonList.RemoveAt(i);
                 FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.SetCursorLock(true);
                 return;
             }
         }
+
+        // Selected the wrong word 
+        StartCoroutine(ShowMessage("Incorrect", 1));
+
     }
 
-
+    IEnumerator ShowMessage(string message, float delay)
+    {
+        MessageText.enabled = true;
+        MessageText.text = message;
+        yield return new WaitForSeconds(delay);
+        MessageText.enabled = false;
+    }
 
 }
---
+++ Assets/Scripts/ObjectBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectBehaviour : MonoBehaviour
+{
+    public float TheDistance;
+    public GameObject ActionDisplay;
+    public GameObject ActionText;
+    public Text MessageText;
+    public GameObject FPC;
+
+    // Update is called once per frame
+    void Update()
+    {
+        TheDistance = PlayerCasting.DistFromTarget;
+    }
+
+    void OnMouseOver()
+    {
+        if (TheDistance <= 3)
+        {
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (TheDistance <= 3)
+            {
+                if (GameManager.wordlist.Contains(PlayerCasting.currentObjectTag))
+                {
+                    FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.SetCursorLock(false);
+                    GameManager.currentLockedObjectTag = PlayerCasting.currentObjectTag;
+                    ActionDisplay.SetActive(false);
+                    ActionText.SetActive(false);
+                    StartCoroutine(ShowMessage("What is this?", 2));
+                    //ThePlayer.SetActive(false)
+                }
+                else
+                {
+                    StartCoroutine(ShowMessage("You have already found this object!",1));
+
+                }
+
+            }
+        }
+    }
+
+    IEnumerator ShowMessage(string message, float delay)
+    {
+        MessageText.enabled = true;
+        MessageText.text = message;
+        yield return new WaitForSeconds(delay);
+        MessageText.enabled = false;
+    }
+
+    void OnMouseExit()
+    {
+        ActionDisplay.SetActive(false);
+        ActionText.SetActive(false);
+    }
+}

d60aaff28b13725cd2d1782118aaeb6372e15b1e
jimmy-gnahz <jimmyjimbozhang@gmail.com>
2019-05-15 13:58:52 +0800

    added flower

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,19 +6,35 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public Button firstWord;
+    public Button secondWord;
+
     public static string currentLockedObjectTag;
     public GameObject FPC;
+    public GameObject WhatIsThisText;
+    private ArrayList wordlist;
+    private ArrayList buttonList;
 
-    public void CheckIfCorrect()
+    public void Start()
     {
-        if (GameManager.currentLockedObjectTag == "tree")
-        {
-            Debug.Log("GameManager.currentLockedObjectTag");
-            Destroy(firstWord);
-            FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.SetCursorLock(true);
+        buttonList = new ArrayList() { firstWord, secondWord };
+        wordlist = new ArrayList(){ "Tree", "Flower"};
+    }
 
+    public void CheckIfCorrect(Text buttonText)
+    {
+        for(int i=0; i<wordlist.Count; i++){
+            if (GameManager.currentLockedObjectTag == (string) wordlist[i] &&
+                GameManager.currentLockedObjectTag == buttonText.text)
+            {
+                WhatIsThisText.SetActive(false);
+                Debug.Log(wordlist[i]);
+                Button currentButton = (Button) buttonList[i];
+                Destroy(currentButton);
+                wordlist.RemoveAt(i);
+                FPC.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().m_MouseLook.SetCursorLock(true);
+                return;
+            }
         }
-
     }
 
 

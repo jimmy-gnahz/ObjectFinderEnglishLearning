9a3c4ad41c96524a8c7df9dd85e7620b409b1922
jimmy-gnahz <jimmyjimbozhang@gmail.com>
2019-05-16 14:26:59 +0800

    Added title screen and menu screen

---
+++ Assets/Scripts/MainMenuFunctions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFunctions : MonoBehaviour
+{
+
+    public AudioSource buttonPress;
+    //public GameObject bestScore;
+
+    public void LoadGame(int index)
+    {
+        if (index == 1)
+        {
+
+        }
+        else if (index == 2)
+        {
+
+        }
+        else if (index == 3)
+        {
+            RedirectManager.redirectToLevel = 2;
+            SceneManager.LoadScene(2);
+        }
+        else if (index == 4)
+        {
+
+        }
+        else if (index == 5)
+        {
+
+        }
+        else if (index == 6)
+        {
+
+        }
+    }
+
+    public void QuitGame()
+    {
+        buttonPress.Play();
+        Application.Quit();
+    }
+}
